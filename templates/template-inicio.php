e69c8ef2c3a62f892b16dc5c400277994165d624
matheusstecker <matheusstecker@live.com>
2025-02-25 14:48:29 -0300

    New Sections

--- templates/template-inicio.php
+++ templates/template-inicio.php
@@ -7,7 +7,7 @@
 get_header();
 ?>
 
-<div class="section-one">
+<section class="section-one">
     <div class="item" id="slide">
         <div class="itens">
             <img src="<?php echo _URLTHEME; ?>/assets/img/slide-1.jpg" alt="Slide 1">
@@ -28,9 +28,9 @@ get_header();
             <img src="<?php echo _URLTHEME; ?>/assets/img/slide-6.jpg" alt="Slide 6">
         </div>
     </div>
-</div>
+</section>
 
-<div class="section-two">
+<section class="section-two">
     <div class="content">
         <div class="left">
             <h1>Nutrição de qualidade para seu pet, com o carinho que ele merece!</h1>
@@ -39,6 +39,42 @@ get_header();
             <a href="" class="btn-light">Compre aqui!</a>
         </div>
     </div>
+</section>
+
+<section class="section-three">
+    <div class="left">
+        <h2>100% Natural</h2>
+        <h2>Prático e saudável!</h2>
+    </div>
+    <div class="right">
+        <img src="<?php echo _URLTHEME; ?>/assets/img/img-produto.png" alt="Imagem Produto PetU">
+    </div>
+</section>
+
+<section class="section-four">
+    <div class="left">
+        <img src="<?php echo _URLTHEME; ?>/assets/img/img-produto.png" alt="Imagem Produto PetU">
+    </div>
+    <div class="right">
+        <h2>Feito por Vets,</h2>
+        <h2>Aprovado por Pets!</h2>
+    </div>
+</section>
+
+<section class="section-five">
+    <div class="left">
+        <img src="<?php echo _URLTHEME; ?>/assets/img/placeholder-youtube.jpg" alt="">
+    </div>
+    <div class="right">
+        <h3>Recomendado pelos Veterinários</h3>
+        <p>Desenvolvida por veterinários e recomendada por especialistas, nossa ração úmida nasceu da necessidade de oferecer uma alimentação de alta qualidade dentro dos hospitais veterinários. Nutrição equilibrada e aprovada por quem mais entende de saúde pet!</p>
+    </div>
+</section>
+
+<div class="section-six">
+    <img src="<?php echo _URLTHEME; ?>/assets/img/icon-pin.svg" alt="Icone Pin">
+    <h3>Compre Pet.U online</h3>
+    <a class="btn" href="">Clique Aqui</a>
 </div>
 
 <?php
