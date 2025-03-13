c0b1a5b81505d76495b9a62791ac2e859d5563fb
matheusstecker <matheusstecker@live.com>
2025-03-13 15:15:19 -0300

    Fix Animations

--- templates/template-inicio.php
+++ templates/template-inicio.php
@@ -32,47 +32,47 @@ get_header();
 
 <section class="section-two">
     <div class="content">
-        <div class="left showLeft">
+        <div class="left hiddenLeft">
             <h1>Nutrição de qualidade para seu pet, com o carinho que ele merece!</h1>
         </div>
-        <div class="right showRight">
+        <div class="right hiddenRight">
             <a href="" class="btn-light">Compre aqui!</a>
         </div>
     </div>
 </section>
 
 <section class="section-three">
-    <div class="left showLeft">
+    <div class="left hiddenLeft">
         <h2>100% Natural</h2>
         <h2>Prático e saudável!</h2>
     </div>
-    <div class="right showRight">
+    <div class="right hiddenRight">
         <img src="<?php echo _URLTHEME; ?>/assets/img/img-produto.png" alt="Imagem Produto PetU">
     </div>
 </section>
 
 <section class="section-four">
-    <div class="left showLeft">
+    <div class="left hiddenLeft">
         <img src="<?php echo _URLTHEME; ?>/assets/img/img-produto.png" alt="Imagem Produto PetU">
     </div>
-    <div class="right showRight">
+    <div class="right hiddenRight">
         <h2>Feito por Vets,</h2>
         <h2>Aprovado por Pets!</h2>
     </div>
 </section>
 
 <section class="section-five">
-    <div class="left showLeft">
+    <div class="left hiddenLeft">
         <img src="<?php echo _URLTHEME; ?>/assets/img/placeholder-youtube.jpg" alt="">
     </div>
-    <div class="right showRight">
+    <div class="right hiddenRight">
         <h3>Recomendado pelos Veterinários</h3>
         <p>Desenvolvida por veterinários e recomendada por especialistas, nossa ração úmida nasceu da necessidade de oferecer uma alimentação de alta qualidade dentro dos hospitais veterinários. Nutrição equilibrada e aprovada por quem mais entende de saúde pet!</p>
     </div>
 </section>
 
 <div class="section-six">
-    <div class="content hiddenBlur showBlur">
+    <div class="content hiddenBlur">
         <img src="<?php echo _URLTHEME; ?>/assets/img/icon-pin.svg" alt="Icone Pin">
         <h3>Compre Pet.U online</h3>
         <a class="btn" href="">Clique Aqui</a>
@@ -81,7 +81,7 @@ get_header();
 
 <div class="section-seven" style="background-image: url('<?php echo _URLTHEME; ?>/assets/img/bg-parallax.jpg');">
     <div class="shadow">
-        <div class="content hiddenBlur showBlur">
+        <div class="content hiddenBlur">
             <img src="<?php echo _URLTHEME; ?>/assets/img/logo-petu.svg" alt="Logo Pet.U">
             <h3>Amor e nutrição em cada refeição!</h3>
         </div>
