4919b301cde4b2af087d4c6a2c000db3cf01dc76
matheusstecker <matheusstecker@live.com>
2025-04-09 10:57:11 -0300

    Add ACF; Add DB

--- templates/template-racoes.php
+++ templates/template-racoes.php
@@ -5,68 +5,57 @@
  */
 
 get_header();
+
+foreach (get_field('conteudo_de_produtos') as $content) {
+
 ?>
 
-<section class="food-one">
-    <div class="left">
-        <img src="<?php echo _URLTHEME; ?>/assets/img/petu-dia-a-dia.png" alt="Imagem do Produto">
-        <div class="icons">
-            <div class="icon">
-                <div class="card">
-                    <img src="<?php echo _URLTHEME; ?>/assets/img/icone-saude-intestinal.svg" alt="Ícone Super Premium">
-                </div>
-                <span>Super Premium</span>
+    <section class="food-one">
+        <div class="left">
+            <div class="img-product">
+                <img src="<?php echo $content['imagem_produto']; ?>" alt="Imagem do Produto">
             </div>
-            <div class="icon">
-                <div class="card">
-                    <img src="<?php echo _URLTHEME; ?>/assets/img/icone-ingrediente-natural.svg" alt="Ícone Rico em Fibras">
-                </div>
-                <span>Rico em Fibras</span>
-            </div>
-            <div class="icon">
-                <div class="card">
-                    <img src="<?php echo _URLTHEME; ?>/assets/img/icone-saboroso-saudavel.svg" alt="Ícone Carnes Nobres">
-                </div>
-                <span>Carnes Nobres</span>
+            <div class="icons">
+                <?php
+                foreach ($content['icones'] as $content_icons) {
+                ?>
+                    <div class="icon">
+                        <div class="card">
+                            <img src="<?php echo $content_icons['imagem_icone']; ?>" alt="Ícone Super Premium">
+                        </div>
+                        <span><?php echo $content_icons['nome_icone']; ?></span>
+                    </div>
+                <?php
+                }
+                ?>
             </div>
         </div>
-    </div>
-    <div class="right">
-        <div class="titles">
-            <h2>Pet.U Dia a Dia - Risoto de carnes com legumes</h2>
-            <p>Alimento completo Pet.U Dia a Dia. Risoto de carne com legumes, pode ser servido puro ou misturado ao alimento seco, tornando a refeição do seu melhor amigo ainda super mais atraente e saborosa. Seu cão merece! Feito com arroz integral. Ingredientes selecionados, frescos e cozidos a vapor</p>
-        </div>
-        <div class="itens">
-            <div class="item">
-                <h3>Modo de usar</h3>
-                <img src="<?php echo _URLTHEME; ?>/assets/img/icone-seta.svg" alt="Ícone Seta">
-                <p>Sirva este alimento puro ou misturado ao alimento de consumo diário na quantidade mínima desejada ou indicada por orientação do seu médico veterinário. Mantenha a higiene dos utensílios utilizados para alimentação, depois de aberto não deixe o alimento úmido por muito tempo fora da refrigeração.</p>
+        <div class="right">
+            <div class="titles">
+                <h2><?php echo $content['nome_produto']; ?></h2>
+                <p><?php echo $content['descricao_produto'];; ?></p>
             </div>
-            <div class="item">
-                <h3>Condições de conservação</h3>
-                <img src="<?php echo _URLTHEME; ?>/assets/img/icone-seta.svg" alt="Ícone Seta">
-                <p>Conservar em local seco, fresco e arejado fora do alcance de calor. Após aberta manter sob refrigeração por no máximo 48 horas. Válido por 2 anos da data de fabricação, seguindo todas as condições de conservação.</p>
-            </div>
-            <div class="item">
-                <h3>Recomendações e restrições</h3>
-                <img src="<?php echo _URLTHEME; ?>/assets/img/icone-seta.svg" alt="Ícone Seta">
-                <p>Recomenda-se o consumo imediato para evitar fermentação no comedouro. Em casos de restrições alimentares ou doenças procure o veterinário para mais informações.</p>
-            </div>
-            <div class="item">
-                <h3>Guia alimentar do cão</h3>
-                <img src="<?php echo _URLTHEME; ?>/assets/img/icone-seta.svg" alt="Ícone Seta">
-                <p>Peso (kg) - Média diária (g)
-1,00 a 2,00kg - de 100g a 180g
-3,00 a 4,00kg - de 180g a 300g
-5,00 a 6,00kg - de 300g a 410g
-7,00 a 8,00kg - de 410g a 510g
-9,00 a 10,0kg - 510g a 600g
-11,0 a 15,0kg - 600g a 820g
-16,0 a 20,0kg - 820g a 1020g </p>
+            <div class="itens">
+                <?php
+                foreach ($content['caracteristicas_produto'] as $content_features) {
+                ?>
+                    <div class="item">
+                        <h3><?php echo $content_features['nome_caracteristica']; ?></h3>
+                        <img src="<?php echo _URLTHEME; ?>/assets/img/icone-seta.svg" alt="Ícone Seta">
+                        <p><?php echo $content_features['descricao_caracteristica']; ?></p>
+                    </div>
+                <?php
+                }
+                ?>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+<?php
+
+}
+
+?>
+
 
 <section class="food-two">
     <div class="left">
