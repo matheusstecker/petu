b65c9c52d4cc4ccd1883cbca09e646156d6a4d20
matheusstecker <matheusstecker@live.com>
2025-04-14 11:55:51 -0300

    FIX: Pages and mobile

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+the_post();
+get_header();
+?>
+
+<section class="container-single">
+    <div class="text-center">
+        <h1>Opss... Não encontramos o que está procurando</h1>
+        <p>Parece que essa página que está procurando não existe ou não está mais disponível</p>
+    </div>
+</section>
+
+<?php
+get_footer();
+?>
--- index.php
+++ index.php
@@ -3,11 +3,8 @@ the_post();
 get_header();
 ?>
 
-<section class="container">
-    <h1><?php echo the_title(); ?></h1>
-    <div class="container-content">
+<section class="container-single">
         <?php echo the_content(); ?>
-    </div>
 </section>
 
 <?php
